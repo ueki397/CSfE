1233a62402bdc9bc1643cac6e711764ab2b5c113
ueki397 <uekijunh522@gmail.com>
2021-03-02 21:25:26 +0900

    bootstrap追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
+  root "home#top"
   get 'users/login_form'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
